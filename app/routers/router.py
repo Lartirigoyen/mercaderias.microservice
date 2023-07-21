074fc9bc42f33e0f0869c069e40b925c4c3df571
kschiebelbein <kschiebelbein@lartirigoyen.com>
2023-07-21 15:54:55 -0300

    Cargo endpoint consulta de partidas a Senasa

--- app/routers/router.py
+++ app/routers/router.py
@@ -28,7 +28,7 @@ async def example(senasa_cod_deposito: int):
   senasa_data = []
   try:
     headers = {'Content-Type': 'text/html'}
-    params = {"authUser": usr_senasa, "authPass": pass_senasa,"userTaxId": tax_id_senasa,"depositId":f"{codigo_senasa}"}
+    params = {"authUser": usr_senasa, "authPass": pass_senasa,"userTaxId": tax_id_senasa,"depositId":f"{senasa_cod_deposito}"}
     url = "https://aps2.senasa.gov.ar/agrotraza/src/api/Consulta_Stock_Producto_Formulado"
     data = requests.get(url, headers=headers, params=params)
     senasa_data = json.loads(data.content)  
